12145a0992b5249765881e4ac729db9597feaf19
Thiago Lima <thenriq@gmail.com>
2020-02-15 12:54:58 +0000

    Added day of the week teller

---
+++ weekday.py
@@ -0,0 +1,14 @@
+# Thiago Lima
+# Telling whether or not today is a weekday.
+
+# Import Python's datetime module
+
+import datetime
+
+weekday = datetime.datetime.today().weekday() # get the day of week. 0 for monday, 6 to sunday
+
+if weekday < 5: # if the day of the week is between monday and friday (0 to 4)
+    print('Yes, unfortunately today is a weekday')
+
+else: # if the day of the week is equal 5 or 6 (saturday or sunday)
+    print('It is the weekend, yay!')
